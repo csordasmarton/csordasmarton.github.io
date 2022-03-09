e4d072315d5099020beebc83fc77b645b6833ce7
Márton Csordás <csordasmarton92@gmail.com>
2022-03-09 22:08:45 +0100

    Change Core to Lead developer in about page

--- src/views/Content/AboutMe.vue
+++ src/views/Content/AboutMe.vue
@@ -7,7 +7,7 @@
     with over <b>+{{ experience }} years</b> of experience. In these days I
     mainly code in <b>Python</b> and <b>Javascript</b> / <b>Vue</b>. Currently
     I am working for <i>Ericsson</i> on the <i>CodeChecker</i> open-source
-    project as a <b>Core developer</b>. I like learning new things and tools
+    project as a <b>Lead developer</b>. I like learning new things and tools
     everyday. If I don't know something I will learn it...
   </Section>
 </template>
